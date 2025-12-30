933adcc1828f33987319fa8c17757bceddac4845
Saai Sudarsanan D <saaisudarsanan@gmail.com>
2025-12-31 02:02:06 +0530

    remove ai infra framework

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1518c7cf.js',
+                path_to_root + 'searchindex-06f8c04c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
